2e13d9e43aaf40b8f9bc9da784ed788db5e04370
Abass-rashid <86113361+Abass-rashid@users.noreply.github.com>
2022-04-03 06:05:17 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <article>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start editing to see some magic happen!</h2>
+        </article>
+      </div>
+    </div>
+  );
+}
